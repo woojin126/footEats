521e7e5ebfb94212e9c5cc3788a0dee3acb32ca3
woojin126 <woojin126@naver.com>
2021-12-02 17:24:13 +0900

    OrderController

--- src/main/java/com/sparta/fooreats/controller/OrderController.java
+++ src/main/java/com/sparta/fooreats/controller/OrderController.java
@@ -4,9 +4,12 @@ import com.sparta.fooreats.dto.OrderRequestDto;
 import com.sparta.fooreats.dto.ResponseOrdered;
 import com.sparta.fooreats.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -17,7 +20,11 @@ public class OrderController {
 
     @PostMapping("/order/request")
     public ResponseOrdered postOrdered(@RequestBody OrderRequestDto orderRequestDto){
-
         return orderService.saveOrder(orderRequestDto);
+    }
+
+    @GetMapping("/orders")
+    public List<ResponseOrdered> findAllByOrdered(){
+        return  orderService.findAllByOrderInfo();
     }
 }
