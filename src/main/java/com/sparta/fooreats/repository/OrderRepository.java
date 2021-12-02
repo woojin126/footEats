f6f38cef3e1f2c69247f5055c0bf5c85bb024371
woojin126 <woojin126@naver.com>
2021-12-02 17:27:49 +0900

    OrderRepository

--- src/main/java/com/sparta/fooreats/repository/OrderRepository.java
+++ src/main/java/com/sparta/fooreats/repository/OrderRepository.java
@@ -11,26 +11,23 @@ import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Ordered,Long> {
 
-/*
 
-    //총가격
-    @Query(value = "select sum(o.totalPrice) from OrderFoods o where o.orders.id in :id ")
-    int findAllMoney(List<Long> id);
-*/
+    //주문 전체정보 조회에서 food정보를 제외한 나머지정보부터 조회
+    @Query(value = "select new com.sparta.fooreats.dto.ResponseOrdered" +
+            " ( o.id, o.restaurant.name, o.restaurant.deliveryFee, o.orderTotalPrice ) " +
+            " from Ordered o " )
+    List<ResponseOrdered> selectAll();
 
-
+    //주문정보 전체조회에 사용하는데, 해당 주문아이디를 바인딩변수로 삽입하여 해당 주문에대한 모든 음식정보를 가져온다.
     @Query(value = " select new com.sparta.fooreats.dto.OrderFood" +
-            "                         (o.orders.id ,f.name, o.quantity , o.totalPrice ) " +
+            "                         (o.orders.id ,f.name, o.totalPrice  ,o.quantity) " +
             "                         from OrderFoods o " +
             "                         join o.foods f " +
             "                         where o.orders.id in :id ")
     List<OrderFood> findFoodInfo(List<Long> id);
 
+    //레스토랑 아이디에 해당하는 모든 주문 아이디 가져오기
     @Query(value = "select o.id from Ordered o where o.restaurant.id = :id" )
     List<Long> findOrderIdCollect(@Param("id") Long id);
-
-/*
-    @Query()
-    ResponseOrdered findResNameAndDeliveryFeeAndTotalPrice();*/
 
 }
