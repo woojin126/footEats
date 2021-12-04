673d077eaa87c1a89f507f64f32b01b615bc57ba
woojin126 <woojin126@naver.com>
2021-12-04 12:41:27 +0900

    Foods Entity

---
+++ src/main/java/com/sparta/fooreats/domain/Foods.java
@@ -0,0 +1,34 @@
+package com.sparta.fooreats.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Foods {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_Id", nullable = false)
+    private Restaurants restaurant;
+
+    @Builder
+    public Foods(Long id,String name, int price, Restaurants restaurant) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.restaurant = restaurant;
+    }
+}
+
